58f8dba7c64748ef1b8bc26e9f36b9aa3527c0a5
Brandon Hoffman <brandonhoffman@gmail.com>
2017-12-06 18:28:42 -0800

    backend complete

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('Anaheim Chili Burger');
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('Lucky Star Burger');
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('Beyond Burger');
+
